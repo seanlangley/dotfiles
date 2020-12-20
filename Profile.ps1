6c73a3bd6bb239e42cf0c850a4dad0edd239d73b
Sean Langley <sean.langley22@gmail.com>
2020-12-20 13:13:24 -0800

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -2,7 +2,10 @@ New-Alias -Name c -Value cd
 New-Alias -Name l -Value ls
 New-Alias -Name which -Value get-command
 Function .. {cd ..}
+Function home {cd $HOME}
 Function gs {git status}
+Function gp {git pull}
 Function ga {git add}
 Function gf {git fetch}
-remove-item alias:curl 
+Function gc {git commit --verbose}
+Function gd {git diff}
