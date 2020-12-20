736925c188af9b5249c9fe867fc92f377932d765
Sean Langley <sean.langley22@gmail.com>
2020-12-20 12:46:07 -0800

    Create Profile.ps1

---
+++ Profile.ps1
@@ -0,0 +1,7 @@
+New-Alias -Name c -Value cd
+New-Alias -Name l -Value ls
+New-Alias -Name which -Value get-command
+Function .. {cd ..}
+Function gs {git status}
+Function ga {git add}
+Function gf {git fetch}
