9fa3f9467ed104abf4725fa7dce11f350288bec0
Sean Langley <sean.langley22@gmail.com>
2021-01-30 18:12:56 -0800

    Add alias to open file with default program

--- Profile.ps1
+++ Profile.ps1
@@ -1,8 +1,10 @@
 # For this script to work, make sure to set the windows script execution policy:
 # Set-ExecutionPolicy Unrestricted
+# Path of Profile.ps1: C:\WINDOWS\system32\WindowsPowerShell\v1.0\Profile.ps1
 New-Alias -Name c -Value cd
 New-Alias -Name l -Value ls
 New-Alias -Name which -Value get-command
+New-Alias -Name open -Value invoke-item 
 Function .. {cd ..}
 Function home {cd $HOME}
 Function gs {git status}
